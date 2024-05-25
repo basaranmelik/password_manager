511da6eb0efdeb99a1ea27c96f733b637d30dfee
Melik <basaranmelik2@gmail.com>
2024-05-25 15:08:49 +0300

    Buttons are activated.

--- main.py
+++ main.py
@@ -108,11 +108,11 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, padx=(27, 0))
 
 # Buttons
-generate_password_button = Button(text="Generate Password", width=14)
+generate_password_button = Button(text="Generate Password", command=generate_password, width=14)
 generate_password_button.grid(row=3, column=2)
-generate_search_button = Button(text="Search", width=14)
+generate_search_button = Button(text="Search", command=search, width=14)
 generate_search_button.grid(row=1, column=2)
-add_button = Button(text="Add", width=36, )
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
